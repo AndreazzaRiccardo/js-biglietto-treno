bf814769c8367ef00268871d0421e143dd6de0da
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-10-18 18:04:26 +0200

    fix: some errors

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // RACCOLTA DATI
 
-const userKm = parseInt(prompt("Quanti kilometri devi percorrere?"));
+let userKm = parseInt(prompt("Quanti kilometri devi percorrere?"));
 let messageKm = ""
+
 if (userKm > 800) {
     messageKm = `${userKm} km!?! Non disponiamo di tratte così lunghe!!!`;
 } else {
@@ -10,8 +11,10 @@ if (userKm > 800) {
 
 const userAge = parseInt(prompt("Quanti anni hai?"));
 let messageAge = ""
+
 if (userAge > 110) {
     messageAge = `${userAge} anni?!? Ma non dovresti essere già morto??`;
+    messageKm = 0
 } else {
     messageAge = userAge
 }
@@ -22,29 +25,34 @@ console.log(messageAge, messageKm);
 
 const priceForKm = 0.21
 const ticketPrice = messageKm * priceForKm
-let messagePrice =""
+let messagePrice = ""
 
 if (isNaN(ticketPrice)) {
-    messagePrice = "Inserire dei dati validi"
+    messagePrice = 0
 } else {
-    messagePrice = ticketPrice.toFixed(2)
+    messagePrice = ticketPrice
 }
 
 // SCONTI
+
 const discountMinors = messagePrice * 0.2
 const discountOver = messagePrice * 0.4
 
 if(userAge < 18) {
-    messagePrice = messagePrice - discountMinors.toFixed(2)
+    messagePrice = messagePrice - discountMinors
 }
 
 if(userAge > 65) {
-    messagePrice = messagePrice - discountOver.toFixed(2)
+    messagePrice = messagePrice - discountOver
 }
 
 console.log(messagePrice);
 
+let finalPrice = `${messagePrice.toFixed(2)} €`
+
+// STAMPA A PAGINA
+
 document.getElementById("kilometri").innerHTML = messageKm
 document.getElementById("age").innerHTML = messageAge
-document.getElementById("total-price").innerHTML = messagePrice
+document.getElementById("total-price").innerHTML = finalPrice
 
