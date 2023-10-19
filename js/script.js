65093b89b63dd9efc5aae9e640e7035957bf8bca
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-10-19 10:47:53 +0200

    fix: exercise correction

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 // RACCOLTA DATI
 
-let userKm = parseInt(prompt("Quanti kilometri devi percorrere?"));
-let messageKm = ""
+const userKm = parseInt(prompt("Quanti kilometri devi percorrere?"));
 
-if (userKm > 800) {
-    messageKm = `${userKm} km!?! Non disponiamo di tratte così lunghe!!!`;
-} else {
-    messageKm = userKm
+if (userKm > 9999) {
+    alert(`${userKm} km!?! Non disponiamo di tratte così lunghe!!!`);
+    userAge = window.location.reload()
+} else if (isNaN(userKm)) {
+    alert("Inserire un valore corretto")
+    userAge = window.location.reload()
 }
 
 const userAge = parseInt(prompt("Quanti anni hai?"));
-let messageAge = ""
 
 if (userAge > 110) {
-    messageAge = `${userAge} anni?!? Ma non dovresti essere già morto??`;
-    messageKm = 0
-} else {
-    messageAge = userAge
+    alert(`${userAge} anni?!? Ma non dovresti essere già morto??`);
+    window.location.reload()
+} else if (isNaN(userAge)) {
+    alert("Inserire un valore corretto")
+    userAge = window.location.reload()
 }
 
-console.log(messageAge, messageKm);
+console.log(userAge, userKm);
 
 // LOGICA DELLA SOLUZIONE
 
 const priceForKm = 0.21
-const ticketPrice = messageKm * priceForKm
-let messagePrice = ""
-
-if (isNaN(ticketPrice)) {
-    messagePrice = 0
-} else {
-    messagePrice = ticketPrice
-}
+const ticketPrice = userKm * priceForKm
+let messagePrice = ticketPrice
 
 // SCONTI
 
@@ -40,10 +35,10 @@ const discountOver = messagePrice * 0.4
 
 if(userAge < 18) {
     messagePrice = messagePrice - discountMinors
-}
-
-if(userAge > 65) {
+    document.getElementById("discount").innerHTML = `Congratulazioni !!! Hai diritto ad uno sconto di ${discountMinors.toFixed(2)} €`
+} else if (userAge > 65) {
     messagePrice = messagePrice - discountOver
+    document.getElementById("discount").innerHTML = `Congratulazioni !!! Hai diritto ad uno sconto di ${discountOver.toFixed(2)} €`
 }
 
 console.log(messagePrice);
@@ -52,7 +47,8 @@ let finalPrice = `${messagePrice.toFixed(2)} €`
 
 // STAMPA A PAGINA
 
-document.getElementById("kilometri").innerHTML = messageKm
-document.getElementById("age").innerHTML = messageAge
+document.getElementById("kilometri").innerHTML = userKm
+document.getElementById("age").innerHTML = userAge
 document.getElementById("total-price").innerHTML = finalPrice
+
 
